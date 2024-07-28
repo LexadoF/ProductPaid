0b86892b10bb902099da1b0b8fad0c93dad1961c
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-07-27 21:31:41 -0500

    updated startup configs
    - Initialized the datasource in the main.ts
    - Using the port in the .env file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { appDefaultPort } from './infrastructure/database/enviromental.config';
+import { initializeDataSource } from './infrastructure/database/typeorm.config';
 
 async function bootstrap() {
+  await initializeDataSource();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(appDefaultPort);
 }
 bootstrap();
