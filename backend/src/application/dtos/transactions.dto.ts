915bb120f6b6d34f6c65877bba8d2e094ceb88fa
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-07-30 09:49:10 -0500

    added transactions dto and enums

---
+++ backend/src/application/dtos/transactions.dto.ts
@@ -0,0 +1,56 @@
+import { Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsObject,
+  IsString,
+  MaxLength,
+  Min,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
+
+class CardDto {
+  @IsNotEmpty()
+  @IsString()
+  number: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(4)
+  cvc: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(2)
+  exp_month: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(2)
+  exp_year: string;
+
+  @IsNotEmpty()
+  @IsString()
+  card_holder: string;
+}
+
+export class CreateTransactionDto {
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
+  product_ammount: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  product_id: number;
+
+  @IsNotEmpty()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => CardDto)
+  card: CardDto;
+}
