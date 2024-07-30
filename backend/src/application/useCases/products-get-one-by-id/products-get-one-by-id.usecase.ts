887786a3ac6c5cb0de24c1f13ed4d71e660c9d20
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-07-29 20:50:28 -0500

    removing any from the code

--- backend/src/application/useCases/products-get-one-by-id/products-get-one-by-id.usecase.ts
+++ backend/src/application/useCases/products-get-one-by-id/products-get-one-by-id.usecase.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from '../../../infrastructure/services/products/products.service';
+import { ProductModel } from 'src/infrastructure/database/models/product.model';
 
 @Injectable()
 export class ProductsGetOneByIdUsecase {
   constructor(private productsService: ProductsService) {}
 
-  async execute(id: number): Promise<any> {
+  async execute(id: number): Promise<ProductModel> {
     const product = await this.productsService.getProductById(id);
     return product;
   }
--- backend/src/application/useCases/products-list/products-list.usecase.ts
+++ backend/src/application/useCases/products-list/products-list.usecase.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from '../../../infrastructure/services/products/products.service';
+import { ProductModel } from 'src/infrastructure/database/models/product.model';
 
 @Injectable()
 export class ProductsListUsecase {
   constructor(private productsService: ProductsService) {}
 
-  async execute(): Promise<any> {
+  async execute(): Promise<ProductModel[]> {
     const productsList = await this.productsService.getAllProducts();
     return productsList;
   }
