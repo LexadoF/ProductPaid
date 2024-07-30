e588ef9670f3406216806ae1914cc6f887a77048
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-07-30 09:56:10 -0500

    Updated delivery models to reflect correctly the data

--- backend/src/infrastructure/database/models/delivery.model.ts
+++ backend/src/infrastructure/database/models/delivery.model.ts
@@ -6,5 +6,26 @@ export class DeliveryModel {
   id: number;
 
   @Column('varchar', { nullable: false })
-  address: string;
+  addressL1: string;
+
+  @Column('varchar', { nullable: true })
+  addressL2: string;
+
+  @Column('varchar', { nullable: false, default: 'CO' })
+  country: string;
+
+  @Column('varchar', { nullable: false })
+  city: string;
+
+  @Column('varchar', { nullable: false })
+  phone_number: string;
+
+  @Column('varchar', { nullable: false })
+  region: string;
+
+  @Column('varchar', { nullable: true })
+  name: string;
+
+  @Column('varchar', { nullable: true })
+  postal_code: string;
 }
