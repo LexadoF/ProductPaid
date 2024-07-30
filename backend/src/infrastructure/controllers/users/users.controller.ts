10b8d71611ed2351df23e085a565ed4289fa5f55
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-07-29 21:38:11 -0500

    defined the structure of the controller for users

--- backend/src/infrastructure/controllers/users/users.controller.ts
+++ backend/src/infrastructure/controllers/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post('/create')
+  createUser(@Body() newUser: any) {
+    console.log(newUser);
+    return 'create works';
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: number, @Body() updatedUserFields: any) {
+    return `${id}  ${updatedUserFields}`;
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: number) {
+    return id;
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: number) {
+    return id;
+  }
+}
