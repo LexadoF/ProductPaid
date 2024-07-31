767b61b047df7c8b9de6d82bf55224d26c26bd90
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-07-30 20:25:18 -0500

    added shipping to the dto

--- backend/src/application/dtos/transactions.dto.ts
+++ backend/src/application/dtos/transactions.dto.ts
@@ -1,8 +1,10 @@
 import { Type } from 'class-transformer';
 import {
+  IsIn,
   IsNotEmpty,
   IsNumber,
   IsObject,
+  IsOptional,
   IsPhoneNumber,
   IsString,
   Max,
@@ -59,6 +61,44 @@ export class CustomerDto {
   legal_id_type: string;
 }
 
+export class shippingDto {
+  @IsString()
+  @IsNotEmpty()
+  address_line_1: string;
+
+  @IsString()
+  @IsOptional()
+  address_line_2?: string;
+
+  @IsString()
+  @IsIn(['CO'])
+  @IsNotEmpty()
+  country: string;
+
+  @IsString()
+  @IsIn(['Cundinamarca', 'Antioquia'])
+  @IsNotEmpty()
+  region: string;
+
+  @IsString()
+  @IsIn(['Bogotá', 'Medellín'])
+  @IsNotEmpty()
+  city: string;
+
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsPhoneNumber('CO')
+  phone_number: string;
+
+  @IsString()
+  @IsOptional()
+  postal_code?: string;
+}
+
 export class CreateTransactionDto {
   @IsNotEmpty()
   @IsNumber()
@@ -82,12 +122,18 @@ export class CreateTransactionDto {
   customer_data: CustomerDto;
 
   @IsNotEmpty()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => shippingDto)
+  shipping_address: shippingDto;
+
+  @IsNotEmpty()
   @IsString()
   acceptance_token: string;
 
   @IsNotEmpty()
   @IsNumber()
   @Min(1)
-  @Max(48)
+  @Max(36)
   installments: number;
 }
