bee144f43daf9fe149f5a31f49d07fbbad31edb3
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-07-30 20:37:51 -0500

    updated the abstract repository for integration

--- backend/src/domain/repository/integration/integration-abstraction.repository.ts
+++ backend/src/domain/repository/integration/integration-abstraction.repository.ts
@@ -1,4 +1,17 @@
+import {
+  CardDto,
+  CustomerDto,
+} from '../../../application/dtos/transactions.dto';
+
 export abstract class integrationAbstractionRepository {
-  abstract createPaymentWP(): any;
+  abstract createPaymentWP(
+    transactionNumber: string,
+    delivery: number,
+    cartToDebit: CardDto,
+    acceptance_token: string,
+    subtotal: number,
+    customerData: CustomerDto,
+    installments: number,
+  ): Promise<any>;
   abstract checkPaymentStatus(): any;
 }
