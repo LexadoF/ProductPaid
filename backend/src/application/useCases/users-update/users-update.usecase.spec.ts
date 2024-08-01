867d4242f104272201f693cb47aee8b9f6cf41ff
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-08-01 12:02:37 -0500

    updated users test cases

--- backend/src/application/useCases/users-update/users-update.usecase.spec.ts
+++ backend/src/application/useCases/users-update/users-update.usecase.spec.ts
@@ -1,7 +1,76 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { UsersUpdateUsecase } from './users-update.usecase';
+import { UsersService } from '../../../infrastructure/services/users/users.service';
+import { UpdateUserDto } from '../../../application/dtos/user.dto';
+import { CustomerModel } from '../../../infrastructure/database/models/customer.model';
+
+const mockUsersService = {
+  updateUser: jest.fn(),
+};
 
 describe('UsersUpdateUsecase', () => {
+  let usecase: UsersUpdateUsecase;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let usersService: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersUpdateUsecase,
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
+    }).compile();
+
+    usecase = module.get<UsersUpdateUsecase>(UsersUpdateUsecase);
+    usersService = module.get<UsersService>(UsersService);
+  });
+
   it('should be defined', () => {
-    expect(new UsersUpdateUsecase()).toBeDefined();
+    expect(usecase).toBeDefined();
+  });
+
+  describe('execute', () => {
+    it('should call updateUser with correct email and updatedUserFields and return updated user', async () => {
+      const email = 'test@example.com';
+      const updatedUserFields: UpdateUserDto = {
+        name: 'New Name',
+        address: 'New Address',
+      };
+      const updatedUser: CustomerModel = {
+        id: 1,
+        name: 'New Name',
+        email: 'test@example.com',
+        address: 'New Address',
+        password: 'hashed-password',
+      };
+
+      jest.spyOn(mockUsersService, 'updateUser').mockResolvedValue(updatedUser);
+
+      expect(await usecase.execute(email, updatedUserFields)).toEqual(
+        updatedUser,
+      );
+      expect(mockUsersService.updateUser).toHaveBeenCalledWith(
+        email,
+        updatedUserFields,
+      );
+    });
+
+    it('should handle errors from UsersService', async () => {
+      const email = 'test@example.com';
+      const updatedUserFields: UpdateUserDto = {
+        name: 'New Name',
+        address: 'New Address',
+      };
+      const error = new Error('Update failed');
+
+      jest.spyOn(mockUsersService, 'updateUser').mockRejectedValue(error);
+
+      await expect(usecase.execute(email, updatedUserFields)).rejects.toThrow(
+        error,
+      );
+    });
   });
 });
