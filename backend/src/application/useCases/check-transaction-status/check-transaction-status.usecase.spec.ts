82efd7ca4a1b29090f905c6ea516849056a54d6d
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-08-01 11:45:40 -0500

    updated use cases tests

--- backend/src/application/useCases/check-transaction-status/check-transaction-status.usecase.spec.ts
+++ backend/src/application/useCases/check-transaction-status/check-transaction-status.usecase.spec.ts
@@ -1,7 +1,19 @@
 import { CheckTransactionStatusUsecase } from './check-transaction-status.usecase';
+import { TransactionsService } from '../../../infrastructure/services/transactions/transactions.service';
 
 describe('CheckTransactionStatusUsecase', () => {
+  let usecase: CheckTransactionStatusUsecase;
+  let transactionsService: jest.Mocked<TransactionsService>;
+
+  beforeEach(() => {
+    transactionsService = {
+      checkTransaction: jest.fn(),
+    } as unknown as jest.Mocked<TransactionsService>;
+
+    usecase = new CheckTransactionStatusUsecase(transactionsService);
+  });
+
   it('should be defined', () => {
-    expect(new CheckTransactionStatusUsecase()).toBeDefined();
+    expect(usecase).toBeDefined();
   });
 });
