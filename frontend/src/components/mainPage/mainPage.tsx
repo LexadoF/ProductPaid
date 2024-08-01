644a6a59b812fd2817c25cd6a2e0d4868d17cdce
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-07-31 22:55:27 -0500

    added the main page template to list the products

--- frontend/src/components/mainPage/mainPage.tsx
+++ frontend/src/components/mainPage/mainPage.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { URL_BASE } from '../../shared/constants/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../app/store';
+import { useNavigate } from 'react-router-dom';
 
 const MainPage: React.FC = () => {
+  const [productList, setProductList] = useState(Array());
+  const isAuth = useSelector((state: RootState) => state.auth.isAuthenticated)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (!isAuth) navigate('/store');
+    axios.get(`${URL_BASE}products`).then((res) => {
+      if (res.status === 200) {
+        setProductList(res.data);
+      }
+    });
+  }, []);
+
+  console.log(productList)
   return (
-    <h1>WORKS</h1>
+    <>
+      <div className='main'>
+        {productList && productList.map((product) => {
+          return (
+            <div id='product-card' key={product.id}>
+              <img src={product.image} alt="product-img" />
+              <div >{product.name} </div>
+              <div >{product.description} </div>
+              <div >{product.price} </div>
+              <div >{product.stock} </div>
+            </div>
+          )
+        })}
+      </div>
+    </>
   );
 };
 
