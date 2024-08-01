9357c3a1ca9c4376de9d3542a3237c1a0e5cea36
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-08-01 12:50:19 -0500

    update integration test cases

--- backend/src/domain/repository/transaction/transaction.repository.spec.ts
+++ backend/src/domain/repository/transaction/transaction.repository.spec.ts
@@ -1,7 +1,60 @@
+import { DataSource } from 'typeorm';
 import { TransactionRepository } from './transaction.repository';
+import { DataSourceImpl } from '../../../infrastructure/database/typeorm.config';
 
 describe('TransactionRepository', () => {
+  const mockDataSourceImpl = {
+    getDataSource: jest.fn().mockReturnValue({
+      getRepository: jest.fn().mockReturnValue({
+        findOne: jest.fn(),
+        save: jest.fn(),
+      }),
+      manager: {
+        getRepository: jest.fn().mockReturnValue({
+          findOne: jest.fn(),
+          save: jest.fn(),
+        }),
+      },
+    }) as unknown as DataSource,
+  };
+  let repository: TransactionRepository;
+
+  beforeEach(() => {
+    repository = new TransactionRepository(
+      mockDataSourceImpl as unknown as DataSourceImpl,
+    );
+  });
+
   it('should be defined', () => {
-    expect(new TransactionRepository()).toBeDefined();
+    expect(repository).toBeDefined();
+  });
+
+  describe('TransactionRepository', () => {
+    let repository: TransactionRepository;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    let mockDataSource: jest.Mocked<DataSource>;
+
+    beforeEach(() => {
+      mockDataSource = {
+        getRepository: jest.fn().mockReturnValue({
+          findOne: jest.fn().mockResolvedValue({ id: 1, price: 100 }),
+          save: jest.fn().mockResolvedValue({ id: 1 }),
+        }),
+        manager: {
+          getRepository: jest.fn().mockReturnValue({
+            findOne: jest.fn().mockResolvedValue({ id: 1 }),
+            save: jest.fn().mockResolvedValue({ id: 1 }),
+          }),
+        },
+      } as unknown as jest.Mocked<DataSource>;
+
+      repository = new TransactionRepository(
+        mockDataSourceImpl as unknown as DataSourceImpl,
+      );
+    });
+
+    it('should be defined', () => {
+      expect(repository).toBeDefined();
+    });
   });
 });
